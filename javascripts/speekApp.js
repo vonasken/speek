acafd7d470acf4657c44e63fb3ad3077725f4308
Marc Von Asken <vonasken@gmail.com>
2016-12-27 10:13:13 -0500

    First Mean Stack commit

---
+++ javascripts/speekApp.js
@@ -0,0 +1,13 @@
+var app = angular.module('speekApp', []);
+
+app.controller('mainController', function($scope){
+   $scope.posts = [];
+   $scope.newPost = {created_by: '', text: '', create_at: ''};
+
+  $scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
+
+});
